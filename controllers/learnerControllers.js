0e4189f03b4b8efad597fb9301e2da3086a23b90
unknown <tieudu1998@gmail.com>
2019-12-01 22:18:48 +0700

    thêm api đăng ký người học type = facebook, google

--- controllers/learnerControllers.js
+++ controllers/learnerControllers.js
@@ -2,15 +2,15 @@ const learnerModel = require("../models/learners");
 
 exports.registerPost = async (req, res) => {
   if (req.params.type === 'normal') {
-    const _email = await learnerModel.findLearnerByEmailType(req.body.email,req.params.type);
+    const _email = await learnerModel.findLearnerByEmailType(req.body.email, req.params.type);
 
     if (_email) {
       return res.json({
         status: 444,
-        message: "existed email"
+        message: "existed account"
       });
     } else {
-      await learnerModel.insertLearner(req.body,req.params.type);
+      await learnerModel.insertLearner(req.body, req.params.type);
 
       return res.json({
         message: "success"
@@ -18,16 +18,33 @@ exports.registerPost = async (req, res) => {
     }
   }
 
-  if(req.params.type==='facebook'){
-    const _id = await learnerModel.findLearnerByEmail(req.body.email);
+  if (req.params.type === 'facebook') {
+    const _idFb = await learnerModel.findLearnerByIdFb(req.body.idFb);
+
+    if (_idFb) {
+      return res.json({
+        status: 444,
+        message: "existed account"
+      });
+    } else {
+      await learnerModel.insertLearner(req.body, req.params.type);
+
+      return res.json({
+        message: "success"
+      });
+    }
+  }
+
+  if (req.params.type === 'google') {
+    const _email = await learnerModel.findLearnerByEmailType(req.body.email, req.params.type);
 
     if (_email) {
       return res.json({
         status: 444,
-        message: "existed email"
+        message: "existed account"
       });
     } else {
-      await learnerModel.insertLearner(req.body);
+      await learnerModel.insertLearner(req.body, req.params.type);
 
       return res.json({
         message: "success"
@@ -36,8 +53,6 @@ exports.registerPost = async (req, res) => {
   }
 
   return res.json({
-    message: "không nhận được type"
+    message: "failed"
   });
-
-
 };
